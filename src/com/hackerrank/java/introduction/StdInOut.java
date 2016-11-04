e206fadccdaa615ca234facb3bdd931096918d0d
Ana Jianu <ana.jianu@yahoo.com>
2016-11-04 19:36:30 +0200

    add description in comment also

--- src/com/hackerrank/java/introduction/StdInOut.java
+++ src/com/hackerrank/java/introduction/StdInOut.java
@@ -3,7 +3,9 @@ package com.hackerrank.java.introduction;
 import java.util.Scanner;
 
 /**
- * Created by ana on 04/11/16.
+ * In this challenge, you must read  integers from stdin and then print them to stdout.
+ * Each integer must be printed on a new line. To make the problem a little easier, a
+ * portion of the code is provided for you in the editor below.
  */
 public class StdInOut {
 
